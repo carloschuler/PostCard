4d63d03320e928fbf71e831c3b44c5f5858a8a01
Carlos Schuler <carlos@reckless.local>
2014-09-14 21:14:01 -0500

    added name function

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var enterMessageTextField: UITextField!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,10 +33,14 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPushed(sender: UIButton)
     {
         messageLabel.hidden = false
+        nameLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
+        nameLabel.text = enterNameTextField.text
         enterMessageTextField.text = ""
+        enterNameTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.textColor = UIColor.blueColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     
     }
